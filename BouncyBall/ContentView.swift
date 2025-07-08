8c0e0d9fd8bb0b5c1a83e74c1cc2b965b2000ae0
mike <miichalp@gmail.com>
2025-07-08 15:46:17 +0800

    Initial Commit

---
+++ BouncyBall/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  BouncyBall
+//
+//  Created by Mike Pyrka on 08.07.2025.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
